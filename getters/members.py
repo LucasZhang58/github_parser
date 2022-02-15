ed438e10ea97282d7f2292dae14952af1cde1fa7
Lucas Zhang <Lucas.Zhang58@gmail.com>
2022-02-15 16:40:01 -0600

    added stuff to the members.py, it isn't finish yet though.
    Signed-off-by: Lucas Zhang <Lucas.Zhang58@gmail.com>

--- getters/members.py
+++ getters/members.py
@@ -25,10 +25,43 @@ def get_MemberEvent(repo_name, created_at, json_payload, record_d, db, member_pa
 
 	# save in the database
 	try:
-		db.add_member(full_repo_name, login, 'members', members_dict[full_repo_name])
+		db.add_member(full_repo_name, record_d['member']['login'], 'members', members_dict[full_repo_name])
 	except Exception as e:
 		print("Failed to save %s MemberEvent record at %s: %s" % \
 				(full_repo_name, created_at, str(e)))
 		traceback.print_exc()
-	
+	#If memebr is of type user,
+
+
+
+
+	try:
+		if record_d['type'] == 'User':
+			if isinstance(record_d['member'], dict):
+				p_dict = record_d['member']
+				persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
+			else:
+				print("record_d: " + str(record_d))
+				print("HAVEN'T FOUND P_DICT")
+	except KeyError as ke:
+		p_dict = None
+		print('p_dict: ' + str(p_dict))
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)	
+		print('KEYERROR RELEASEEVENT_EXCEPTION: ' + str(ke))
+		exit(1)
+	except Exception as e:
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)	
+		print('ERROR RELEASEEVENT_EXCEPTION: ' + str(e))
+		traceback.print_exc()
+		exit(1)
+
+	if isinstance(p_dict, dict):
+		persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
+	else:
+		print('p_dict: ' + str(p_dict))
+		print('P_DICT IS NOT A DICT!!!!!!!!!!!!!!!!')
+
+	#typ
 
