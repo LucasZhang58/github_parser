519f1f60bbae0c342441e9f4eb7ecb94880a6f61
Ashish Bijlani <ashish@ossillate.com>
2022-02-15 07:17:59 -0600

    Fix refs....still not working
    Signed-off-by: Ashish Bijlani <ashish@ossillate.com>

--- getters/refs.py
+++ getters/refs.py
@@ -10,212 +10,156 @@ import getters.name as name
 ##########################
 # Create Events
 ##########################
-def get_attributes(json_payload, record_d):
-        try:
-                try:
-                        type_name = json_payload['ref']
-                except KeyError as ke:
-                        try:
-                                type_name = record_d['ref']
-                        except KeyError as ke:
-                                try:
-                                        type_name = record_d['name']
-                                except KeyError as ke:
-                                        type_name = None
-                try:
-                        return type_name, json_payload['created_at']
-                except KeyError as ke:
-                        record_created_at = None
-                        return type_name, record_created_at
-        except Exception as e:
-                print('record_d: ' + str(record_d))
-                frameinfo = getframeinfo(currentframe())
-                print(frameinfo.filename, frameinfo.lineno)	
-                print('get_attributes CREATEEVENT_EXCEPTION: ' + str(e))
-                traceback.print_exc()
-                exit(1)
+def get_attributes(attr, json_payload, record_d, created_at):
+	try:
+		try:
+			type_name = json_payload[attr]
+		except KeyError as ke:
+			try:
+				type_name = record_d[attr]
+			except KeyError as ke:
+				raise Exception("attr %s not found!" % (attr))
+
+		return type_name, created_at
+
+	except Exception as e:
+		print('record_d: ' + str(record_d))
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)	
+		print('get_attributes CREATEEVENT_EXCEPTION: ' + str(e))
+		traceback.print_exc()
+		exit(1)
+
+def parse_ref_type(attr, ref_type, repo_name, created_at, json_payload, record_d, db, ref_past_repo_names):
+
+	if ref_type in ['branch', 'object', 'repository']:
+		branch_name, record_created_at = get_attributes(attr, json_payload, record_d, created_at)
+		if ref_type == 'repository':
+			if not branch_name:
+				branch_name = 'master'
+			ref_type = 'branch'
+		ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, ref_type)
+
+	elif ref_type == 'tag':
+		tag_name, record_created_at = get_attributes(attr, json_payload, record_d, created_at)
+		ref_helper(repo_name, created_at, record_d, json_payload, db, tag_name, record_created_at, ref_past_repo_names, ref_type)
+
+	else:
+		raise Exception("Unkown %s in json_payload: %s" % (ref_type))
 
 def get_CreateEvent(repo_name, created_at, json_payload, record_d, db, ref_past_repo_names):
-        try:
-                try:
+	try:
+		if 'ref_type' in json_payload:
+			parse_ref_type('ref', json_payload['ref_type'], repo_name, created_at, json_payload, record_d, db, ref_past_repo_names)
+		elif 'ref_type' in record_d:
+			parse_ref_type('ref', record_d['ref_type'], repo_name, created_at, json_payload, record_d, db, ref_past_repo_names)
+		elif 'object' in json_payload:
+			parse_ref_type('object_name', json_payload['object'], repo_name, created_at, json_payload, record_d, db, ref_past_repo_names)
+		else:
+			raise Exception('ref_type or object not found!')
 
-                        if json_payload['ref_type'] == 'branch':
-                                branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')
-
-                except KeyError as ke:
-                        try:
-                                if json_payload['object'] == 'branch':
-                                        branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                        ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')
-
-                        except KeyError as ke:
-                                try:
-                                        if record_d['ref_type'] == 'branch':
-                                                branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                                ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')
-                                except KeyError as ke:
-                                        frameinfo = getframeinfo(currentframe())
-                                        print(frameinfo.filename, frameinfo.lineno)	
-                                        print('record_d: ' + str(record_d) + ' KEYERROR: ' + print(ke))
-
-                try:
-                        if json_payload['ref_type'] == 'tag':
-                                tag_name, record_created_at = get_attributes(json_payload, record_d)
-                                ref_helper(repo_name, created_at, record_d, json_payload, db, tag_name, record_created_at, ref_past_repo_names, 'tag')
-                except KeyError as ke:	
-                        try:
-                                if json_payload['object'] == 'tag':
-                                        branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                        ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')		
-                        except KeyError as ke:
-                                try:
-                                        if record_d['ref_type'] == 'tag':
-                                                branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                                ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')	
-                                except KeyError as ke:
-                                        frameinfo = getframeinfo(currentframe())
-                                        print(frameinfo.filename, frameinfo.lineno)	
-                                        print('record_d: ' + str(record_d) + ' KEYERROR: ' + print(ke))
-
-                try:
-                        if json_payload['ref_type'] == 'repository':
-                                tag_name, record_created_at = get_attributes(json_payload, record_d)
-                                ref_helper(repo_name, created_at, record_d, json_payload, db, tag_name, record_created_at, ref_past_repo_names, 'tag')
-                except KeyError as ke:	
-                        try:
-                                if json_payload['object'] == 'repository':
-                                        branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                        ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')		
-                        except KeyError as ke:
-                                try:
-                                        if record_d['ref_type'] == 'repository':
-                                                branch_name, record_created_at = get_attributes(json_payload, record_d)
-                                                ref_helper(repo_name, created_at, record_d, json_payload, db, branch_name, record_created_at, ref_past_repo_names, 'branch')	
-                                except KeyError as ke:
-                                        frameinfo = getframeinfo(currentframe())
-                                        print(frameinfo.filename, frameinfo.lineno)	
-                                        print('record_d: ' + str(record_d) + ' KEYERROR: ' + print(ke))
-        except Exception as e:
-                print('record_d: ' + str(record_d) + ' is of type ' + str(record_d))
-                frameinfo = getframeinfo(currentframe())
-                print(frameinfo.filename, frameinfo.lineno)	
-                print('get_CreateEvent CREATEEVENT_EXCEPTION: ' + str(e))
-                traceback.print_exc()
-                exit(1)
+	except Exception as e:
+		print('record_d: ' + str(record_d) + ' is of type ' + str(record_d))
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)	
+		print('get_CreateEvent CREATEEVENT_EXCEPTION: ' + str(e))
+		traceback.print_exc()
+		exit(1)
 
 def ref_helper(repo_name, created_at, record_d, json_payload, db, type_name, record_created_at, ref_past_repo_names, ref_type):
-        try:
-                full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
+	try:
+		full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
 
-                #create type_dict to append to the branches, tag, or repository key-value pair
-                type_dict = {}
-                if record_created_at:
-                        type_dict['record_created_at'] = record_created_at
-                if type_name:
-                        try:
-                                type_dict['{}_name'.format(ref_type)] = type_name
-                        except KeyError as ke:
-                                print('re_helper, type_name Error: ' + str(ke))
-                ref_type_plural = ''
-                if ref_type == 'branch':
-                        ref_type_plural = 'branches'
+		#create type_dict to append to the branches, tag, or repository key-value pair
+		type_dict = {}
+		if record_created_at:
+			type_dict['record_created_at'] = record_created_at
+		if type_name:
+			try:
+				type_dict['{}_name'.format(ref_type)] = type_name
+			except KeyError as ke:
+				print('re_helper, type_name Error: ' + str(ke))
 
-                if ref_type == 'tag':
-                        ref_type_plural = 'tags'
+		ref_type_plural = ''
+		if ref_type == 'branch':
+			ref_type_plural = 'branches'
+		elif ref_type == 'tag':
+			ref_type_plural = 'tags'
+		else:
+			raise Exception("Unknown ref_type %s" % (ref_type))
 
-                if ref_type == 'repository':
-                        ref_type_plural = 'repositories'
+		#try:
+		#	if  full_repo_name not in  ref_past_repo_names:
+		#		ref_past_repo_names[full_repo_name] = {ref_type_plural : [type_dict]}
+		#		#print(ref_past_repo_names['{}'.format(full_repo_name)])
+		#	else:
+		#		if ref_type_plural not in ref_past_repo_names[full_repo_name]:
+		#			ref_past_repo_names[full_repo_name][ref_type_plural] = [type_dict]
+		#		else:
+		#			ref_past_repo_names[full_repo_name][ref_type_plural].append(type_dict)
+		#except KeyError as ke:
+		#	print('KEYERROR re_helper, if full_repo_name not in ref_past_repo_names["{}".format(full_repo_name)] and not ref_past_repo_names["{}".format(full_repo_name)]: ' + str(ke))
+		#	traceback.print_exc()
 
-                try:
-                        if  full_repo_name not in  ref_past_repo_names:
-                                ref_past_repo_names[full_repo_name] = {ref_type_plural : [type_dict]}
-                                #print(ref_past_repo_names['{}'.format(full_repo_name)])
-                        else:
-                                if ref_type_plural not in ref_past_repo_names[full_repo_name]:
-                                        ref_past_repo_names[full_repo_name][ref_type_plural] = [type_dict]
-                                else:
-                                        ref_past_repo_names[full_repo_name][ref_type_plural].append(type_dict)
-                except KeyError as ke:
-                        print('KEYERROR re_helper, if full_repo_name not in ref_past_repo_names["{}".format(full_repo_name)] and not ref_past_repo_names["{}".format(full_repo_name)]: ' + str(ke))
-                        traceback.print_exc()
+		# save in the database
+		db.add_data(full_repo_name, created_at, ref_type_plural, type_dict)
 
-                # save in the database
-                try:
-                        db.add_data(full_repo_name, created_at, 'refs', type_dict)
-                except Exception as e:
-                        print("Failed to save %s ref output at %s: %s" % \
-                                        (full_repo_name, created_at, str(e)))
-                        traceback.print_exc()
+		# save repo informtion
+		try:
+			r_dict = record_d['repo']
+		except KeyError as ke:
+			try:
+				r_dict = record_d['repository']
+			except KeyError as ke:
+				raise Exception("'repo' or 'repository' not found")
 
-                # save repo informtion
-                try:
-                        r_dict = record_d['repo']
-                except KeyError as ke:
-                        try:
-                                r_dict = record_d['repository']
-                        except KeyError as ke:
-                                frameinfo = getframeinfo(currentframe())
-                                print(frameinfo.filename, frameinfo.lineno)	
-                                print('KEYERROR CREATEEVENT_EXCEPTION: ' + str(ke))
-                                exit(1)
-                except Exception as e:
-                        frameinfo = getframeinfo(currentframe())
-                        print(frameinfo.filename, frameinfo.lineno)	
-                        traceback.print_exc()
-                        print('ERROR CREATEEVENT_EXCEPTION: ' + str(e))
-                        exit(1)
+		if isinstance(r_dict, dict):
+			repos.get_Repo(full_repo_name, created_at, json_payload, record_d, r_dict, db)
+		else:
+			raise Exception("'r_dict' not a dict!")
 
-                if isinstance(r_dict, dict):
-                        repos.get_Repo(full_repo_name, created_at, json_payload, record_d, r_dict, db)
+		# save person information
+		try:
+			if isinstance(record_d['actor'], dict):
+				p_dict = record_d['actor']
+				persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
+			elif isinstance(record_d['actor_attributes'], dict):
+					p_dict = record_d['actor_attributes']
+					persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
+			else:
+				print("record_d: " + str(record_d))
+				raise Exception("HAVEN'T FOUND P_DICT")
+		except KeyError as ke:
+			# try:
+			#         p_dict = record_d['actor']
+			# except KeyError as ke:
+			p_dict = {
+				'login' : record_d['actor']
+			}
+			print('KEYERROR: ' + str(ke))
+			frameinfo = getframeinfo(currentframe())
+			print(frameinfo.filename, frameinfo.lineno)	
+			print('record_d: ' + str(record_d))
+			traceback.print_exc(0)
+			exit(1)
+		except Exception as e:
+			frameinfo = getframeinfo(currentframe())
+			print(frameinfo.filename, frameinfo.lineno)	
+			print('ERROR CREATEEVENT_EXCEPTION: ' + str(e))
+			traceback.print_exc()
+			exit(1)
 
-                # save person information
-                try:
-                        if isinstance(record_d['actor'], dict):
-                                p_dict = record_d['actor']
-                                persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
-                        elif isinstance(record_d['actor_attributes'], dict):
-                                        p_dict = record_d['actor_attributes']
-                                        persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
-                        else:
-                                print("record_d: " + str(record_d))
-                                print("HAVEN'T FOUND P_DICT")
-                except KeyError as ke:
-                        # try:
-                        #         p_dict = record_d['actor']
-                        # except KeyError as ke:
-                        p_dict = {
-                                'login' : record_d['actor']
-                        }
-                        print('KEYERROR: ' + str(ke))
-                        # frameinfo = getframeinfo(currentframe())
-                        # print(frameinfo.filename, frameinfo.lineno)	
-                        print('record_d: ' + str(record_d))
-                        traceback.print_exc(0)
-                        # exit(1)
-                except Exception as e:
-                        frameinfo = getframeinfo(currentframe())
-                        print(frameinfo.filename, frameinfo.lineno)	
-                        print('ERROR CREATEEVENT_EXCEPTION: ' + str(e))
-                        traceback.print_exc()
-                        exit(1)
+		full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
 
-                full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
-
-                if isinstance(p_dict, dict):
-                        persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
-                else:
-                        print('p_dict: ' + str(p_dict))
-                        print('P_DICT IS NOT A DICT!!!!!!!!!!!!!!!!')
-        except Exception as e:
-                print('record_d: ' + str(record_d) + ' is of type ' + str(record_d))
-                frameinfo = getframeinfo(currentframe())
-                print(frameinfo.filename, frameinfo.lineno)	
-                print('ref_helper CREATEEVENT_EXCEPTION: ' + str(e))
-                traceback.print_exc()
-                exit(1)
-
-
-
-
-        
+		if isinstance(p_dict, dict):
+			persons.get_Person(full_repo_name, created_at, json_payload, record_d, p_dict, db)
+		else:
+			print('p_dict: ' + str(p_dict))
+			print('P_DICT IS NOT A DICT!!!!!!!!!!!!!!!!')
+	except Exception as e:
+		print('record_d: ' + str(record_d) + ' is of type ' + str(record_d))
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)	
+		print('ref_helper CREATEEVENT_EXCEPTION: ' + str(e))
+		traceback.print_exc()
+		exit(1)
--- gzip_main.py
+++ gzip_main.py
@@ -40,7 +40,7 @@ def main():
 	print("Collected %d gzipped files" % (len(gzip_file_list)))
 
 	cfg = config.Config()
-	mode = 'ProcessPool' #cfg.get("QUEUING", "Infrastructure")
+	mode = None #'ProcessPool' #cfg.get("QUEUING", "Infrastructure")
 
 	args = [input_path, db]
 	if mode == "ProcessPool":
