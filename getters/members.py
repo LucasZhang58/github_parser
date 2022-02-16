3440ff8c8844f35d81d1f4874214df5cba17c127
Lucas Zhang <Lucas.Zhang58@gmail.com>
2022-02-16 13:53:01 -0600

    get_MemberEvent() is now fully functioning without errors.
    Signed-off-by: Lucas Zhang <Lucas.Zhang58@gmail.com>

--- getters/members.py
+++ getters/members.py
@@ -5,23 +5,26 @@ import json
 import os
 import sys
 from inspect import currentframe, getframeinfo
-from getters import persons, repos
+from getters import persons, repos, orgs
 import getters.name as name
 
 ##########################
 # Member Events
 ##########################
 
-def get_MemberEvent(repo_name, created_at, json_payload, record_d, db, member_past_repo_names, members_dict):
-	full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
-	user_name_string = name.get_user_name_string(json_payload, record_d, full_repo_name)
 
-	if full_repo_name not in member_past_repo_names:
-		members_dict[full_repo_name] = [record_d]
+def get_person_or_org_from_url(full_repo_name, created_at, json_payload, record_d, m_dict, db):
+	if '/users/' in record_d['actor']['url']:
+		persons.get_Person(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+	elif '/orgs/' in record_d['actor']['url']:
+		orgs.get_Org(full_repo_name, created_at, json_payload, record_d, m_dict, db)
 	else:
-		members_dict[full_repo_name].append(record_d)
-	member_past_repo_names.add(full_repo_name)
+		frameinfo = getframeinfo(currentframe())
+		print(frameinfo.filename, frameinfo.lineno)
+		print('Actor is not a dict!')
+		print('record_d: ' + str(record_d))
 
+def get_m_dict(json_payload, record_d):
 	try:
 		m_dict = record_d['member']
 	except KeyError as ke:
@@ -33,21 +36,59 @@ def get_MemberEvent(repo_name, created_at, json_payload, record_d, db, member_pa
 		print('KEYERROR MEMBEREVENT_EXCEPTION: ' + str(e))
 		traceback.print_exc()
 		exit(1)
+	return m_dict
+	
 
+def get_MemberEvent(repo_name, created_at, json_payload, record_d, db, member_past_repo_names, members_dict):
+	full_repo_name = name.get_full_repo_name(json_payload, record_d, repo_name)
+	user_name_string = name.get_user_name_string(json_payload, record_d, full_repo_name)
 
+	if full_repo_name not in member_past_repo_names:
+		members_dict[full_repo_name] = [record_d]
+	else:
+		members_dict[full_repo_name].append(record_d)
+	member_past_repo_names.add(full_repo_name)
 
-	# # save in the database
-	# try:
-	# 	db.add_member(full_repo_name, m_dict)
-	# 	try:
-	# 		if m_dict['type'] == 'User':
-	# 			persons.get_Person(full_repo_name, created_at, json_payload, record_d, m_dict, db)
-	# except Exception as e:
-	# 	db.add_member(full_repo_name, m_dict)
-	# 	print('record_d: ' + str(record_d))
-	# 	print("Failed to save %s MemberEvent record at %s: %s" % \
-	# 			(full_repo_name, created_at, str(e)))
-	# 	traceback.print_exc()
+	m_dict = get_m_dict(json_payload, record_d)
+
+	# save in the database
+	try:
+		db.add_member(full_repo_name, m_dict)
+		try:
+			if isinstance(m_dict, dict):
+				try:
+					if m_dict['type'] == 'User' or m_dict['type'] == 'user':
+						persons.get_Person(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+					elif m_dict['type'] == 'Org' or m_dict['type'] == 'org':
+						orgs.get_Org(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+					else:
+						print('record_d: ' + str(record_d))
+				except KeyError as ke:
+					try:
+						if '/users/' in m_dict['url']:
+							persons.get_Person(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+						elif '/orgs/' in m_dict['url']:
+							persons.get_Person(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+						else:
+							print('record_d: ' + str(record_d))
+					except KeyError as ke:
+						get_person_or_org_from_url(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+			elif isinstance(m_dict, str):
+				m_dict = {'login' : m_dict}
+				get_person_or_org_from_url(full_repo_name, created_at, json_payload, record_d, m_dict, db)
+			else:
+				print('m_dict is not a dict or a str!')
+		except KeyError as ke:
+			frameinfo = getframeinfo(currentframe())
+			print(frameinfo.filename, frameinfo.lineno)	
+			print('KEYERROR: ' + print(str(ke)))
+			print('record_d: ' + str(record_d))
+	except Exception as e:
+		db.add_member(full_repo_name, m_dict)
+		print('record_d: ' + str(record_d))
+		print("Failed to save %s MemberEvent record at %s: %s" % \
+				(full_repo_name, created_at, str(e)))
+		traceback.print_exc()
 
 
 
